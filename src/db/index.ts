d3b547c70003cc2b86a22a302343fd522628befe
abhishake1 <abhi9848038283@gmail.com>
2024-09-17 06:25:30 +0000

    chore: remove trpc

--- src/db/index.ts
+++ src/db/index.ts
@@ -7,13 +7,11 @@ export const db = typeof window !== "undefined" ? Gun(window.location.origin + "
 
 export function useDb<T>(table: string): [T[], Pick<ReturnType<typeof db.get>, "set">] {
   const [data, setData] = useState<Record<string, T>>({});
-  const t = db.get(table)
+  const handler = db.get(table)
   useEffect(() => {
-    t.map().on((data) => {
-      setData(p => ({ ...p, [data._["#"]]: data }))
-    });
+    handler.map().on((data) => setData(p => ({ ...p, [data._["#"]]: data })));
   }, []);
-  return [Object.values(data), t];
+  return [Object.values(data), handler];
 }
 
 
